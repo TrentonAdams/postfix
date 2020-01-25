5339a329829e3790e2f86d1702af4ef3cb75aae3
Trenton D. Adams <trenton.d.adams@gmail.com>
2020-01-24 19:34:35 -0700

    direct stderr to stdout so we can see errors

--- etc/postfix-init.d/500-s3-config.sh
+++ etc/postfix-init.d/500-s3-config.sh
@@ -2,8 +2,8 @@
 
 if [ ! -z "$s3_bucket" ]; then
     echo starting s3-config sync from bucket: $s3_bucket | logger
-    aws s3 cp s3://$s3_bucket/postfix-conf.tar.gz /tmp/ || \
+    aws s3 cp s3://$s3_bucket/postfix-conf.tar.gz /tmp/ 2>&1 || \
         { echo s3 copy failed | logger; exit 1; }
-    tar -xvzf /tmp/postfix-conf.tar.gz -C / || \
+    tar -xvzf /tmp/postfix-conf.tar.gz -C / 2>&1 || \
         { echo tar extract of configuration files failed | logger; exit 2; }
 fi
